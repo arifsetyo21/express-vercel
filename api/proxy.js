aff667f9c1b80749d112234d4b6d963dbd20ec67
arifsetyo21 <arifsetyo19@gmail.com>
2021-10-13 23:13:54 +0700

    adding endpoint for proxy with params

--- api/proxy.js
+++ api/proxy.js
@@ -5,15 +5,13 @@ const https = require("https");
 
 router.get("/", async (req, res) => {
   try {
+    const url = req.query.url;
     const response = await axios
-      .get(
-        "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote",
-        {
-          httpsAgent: new https.Agent({
-            rejectUnauthorized: false,
-          }),
-        }
-      )
+      .get(`${url}`, {
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      })
       .then(({ data }) => {
         return data;
       });
