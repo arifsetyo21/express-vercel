36ad0423e680b90a80d1d00d7dfa3be78a421074
arifsetyo21 <arifsetyo19@gmail.com>
2021-10-14 22:09:43 +0700

    fix cors

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const proxy = require("./api/proxy");
 app.use(express.json({ extended: false }));
 app.use(cors());
 app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api/product", product);
 app.use("/api/proxy", proxy);
 
-const PORT = process.env.PORT || 8091;
+const PORT = process.env.PORT || 8092;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
